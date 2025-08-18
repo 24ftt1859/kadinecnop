b297bd8dc62b2c1e33eb2acfddc3877b26cc088a
24ftt1859 <24FTT1859@student.pb.edu.bn>
2025-08-18 08:06:05 +0800

    Add files via upload

---
+++ P2/practical_2_4.java
@@ -0,0 +1,7 @@
+public class practical_2_4  {
+    public static void main(String[] args)  {
+        double result = (7.5 * 2.5 - 1.5 * 3) / (4 - 2.5);
+        System.out.printf("The result of 7.5 x 2.5 - 1.5 x 3\n             --------------------- is %.2f",result);
+        System.out.printf("\n                    4 - 2.5");
+    }
+}
